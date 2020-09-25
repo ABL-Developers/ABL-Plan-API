bc0eda983d298958b5bd65dac0246c41f2b46080
abolfazlalz <mrabolfazlalz@gmail.com>
2020-09-25 19:51:44 +0330

    Init codes

--- program.js
+++ program.js
@@ -1,15 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
-const ResponseHelper = require('./utils/ResponseHelper');
+const ResponseHelper = require('./utils/ResponseHelper')
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    let response = new ResponseHelper();
-    response.setStatus(true);
-    response.putData('key', process.env.ACCESS_TOKEN_SECRET);
-    res.send(response.getResponse());
-})
+app.use(express.json())
 
+app.get('/', (req, res) => {
+    let response = new ResponseHelper()
+    const token = req.headers['authorization']
+    let auth = process.env.ACCESS_TOKEN_SECRET
+    if (token != auth) {
+        res.sendStatus(403)
+    }
+    res.send(response.getResponse())
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
--- utils/ResponseHelper.js
+++ utils/ResponseHelper.js
@@ -18,7 +18,7 @@ module.exports = class ResponseHelper {
     }
 
     putData(key, value) {
-        this.response.data.key = value;
+        this.response.data[key] = value;
     }
 
     removeData(key) {
