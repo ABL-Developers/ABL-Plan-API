0dc99f0fdd02017c14ba6b16c27a59bbe217af20
abolfazlalz <mrabolfazlalz@gmail.com>
2020-09-28 10:03:42 +0330

    Update PlansControl.js
    Add createNewPlan method to create new plan for user

--- databaseControl/PlansControl.js
+++ databaseControl/PlansControl.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const EventEmitter = require('events').EventEmitter
 const MongoClient = require('mongodb').MongoClient
 
-const uri = `mongodb+srv://abolfazlalz:${process.env.DB_PASSWORD}@cluster0.homvc.mongodb.net/accounts?retryWrites=true&w=majority`
+const uri = `mongodb+srv://abolfazlalz:${process.env.DB_PASSWORD}@cluster0.homvc.mongodb.net/plans?retryWrites=true&w=majority`
 
 
 module.exports = class PlansControl {
@@ -15,5 +15,43 @@ module.exports = class PlansControl {
 
     getListener() {
         return this.listener
+    }
+
+    createNewPlan(data, userId) {
+        let createdDate = data.createdDate
+        let deadlineDate = createdDate
+        if (data.deadlineDate == undefined)
+            deadlineDate = data.deadlineDate
+
+        let planData = {
+            'title': data.title,
+            'description': data.description,
+            'dates': {
+                'created': date.createdDate,
+                'deadline': deadlineDate,
+                'modified': date.createdDate
+            },
+            "users": {
+                [userId]: ['creator']
+            }
+        }
+
+        this.client.connect(err => {
+            if (err) {
+                this.listener.emit('insert-error')
+                return
+            }
+            const collection = this.client.db("plans").collection("plans");
+            collection.insertOne(planData, callback => {
+                if (callback != undefined && callback.hasErrorLabel)
+                    this.listener.emit("insert-error")
+                else
+                    this.listener.emit("insrt-success")
+            }, error => {
+                this.listener.emit("insert-error")
+            })
+            this.client.close()
+        })
+
     }
 }
